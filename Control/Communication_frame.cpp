822e0b52a8e41ac4580ff5c73582e066f61cf0a5
khchu1993 <ka-hei.chu.1@etsmtl.net>
2016-04-02 16:37:40 -0400

    Update Communication_frame.cpp

--- Control/Communication_frame.cpp
+++ Control/Communication_frame.cpp
@@ -20,14 +20,15 @@ int ArretUrgence = 0;
 int Acceleration = 0;
 int Deceleration = 0;
 int LongueurCable[2] = {0,0};
+int Receiver = 0;
 
 
 
 void setup()  {
-   Serial.begin(115200);
+   Serial.begin(9600);
 }
 
-void loop() {
+void ReceiveUpdate() {
 // Si Xbee a recu quelque
 
 // Trame de demande de connexion: SOF = CXN
@@ -37,150 +38,176 @@ void loop() {
 // msg[2] = SOF[2]
 // msg[3] = Adresse de la manette
 // msg[4] = Checksum
+if (Connexion == 0){
+Serial.println("Not connecte");
+}
+else{
+Serial.println("Connecte");  
+}
+Serial.println(NbBit);
 
-while (Serial.available() > 0 && Connexion == 0 ) { 
+
+if (Serial.available() > 0 && Connexion == 0) { 
   msg[NbBit]=Serial.read();
-  
+  Receiver = 1;
+  Serial.println(NbBit);
+  Serial.println(msg[NbBit]);
   // Trame de demande de connexion 
-  if (NbBit==0){
+  if (NbBit==0 && Receiver == 1){
     if (msg[0]== 'C'){                                                             // si la première lettre = C
       NbBit++;                                                                     // si oui, continuer
+      Receiver = 0;
     }
     else{                                                                          // si non, recommencer
       NbBit=0;
-      for (int i = 0; i = sizeof(msg); i++)                                        // effacer le tableau
-        msg[i] = (char) 0;   
+      Receiver = 0;
     }
   }
-  if (NbBit==1){
-    if (msg[1]== 'X'){                                                             // si la deuxième lettre = X
+  if (NbBit ==1 && Receiver == 1){
+    if (msg[1]== 'X'){                                                             // si la première lettre = C
       NbBit++;                                                                     // si oui, continuer
+      Receiver = 0;
     }
     else{                                                                          // si non, recommencer
       NbBit=0;
-      for (int i = 0; i = sizeof(msg); i++)                                        // effacer le tableau
-        msg[i] = (char) 0;   
+      Receiver = 0;
     }
   }
-  if (NbBit==2){
-    if (msg[2]== 'N'){                                                             // si la troisième lettre = N
+  if (NbBit ==2 && Receiver == 1){
+    if (msg[2]== 'N'){                                                             // si la première lettre = C
       NbBit++;                                                                     // si oui, continuer
+      Receiver = 0;
     }
     else{                                                                          // si non, recommencer
       NbBit=0;
-      for (int i = 0; i = sizeof(msg); i++)                                        // effacer le tableau
-        msg[i] = (char) 0;
+      Receiver = 0;
     }
   }
-  if (NbBit==3){
+  if (NbBit==3 && Receiver == 1){
     NbBit++;
+    Receiver = 0;
   }
-  if (NbBit==4){
+  if (NbBit==4 && Receiver == 1){
     if (msg[4] == '4'){                                                            // si le contrôle a reçu toute la trame
       Connexion = 1;                                                               // si oui, connecter
       ManetteAdresse = msg[3];                                                     // prendre en note l'adresse de la manette connectée
+      Serial.println("Connexion On");
     }
   NbBit=0;                                                                         // recommencer peu importe si on a reçu toute la trame au complête
-  for (int i = 0; i = sizeof(msg); i++)                                            // effacer le tableau
-    msg[i] = (char) 0;
-  }  
+  Receiver = 0;
+  }   
 }
 
-while (Serial.available() > 0 && Connexion == 1 ) {
+if (Serial.available() > 0 && Connexion == 1 ) {
   msg[NbBit]=Serial.read();
+  Receiver = 1;
+  Serial.println(NbBit);
+  Serial.println(msg[NbBit]);
 
-  if (NbBit==0){
+  if (NbBit==0 && Receiver == 1){
     if (msg[0]== 'C' || msg[0]== 'D'){                                             // si la première lettre = C ou D
       NbBit++;                                                                     // si oui, continuer
+      Receiver = 0;
     }
     else{                                                                          // si non, recommencer
       NbBit=0;
-      for (int i = 0; i = sizeof(msg); i++)                                        // effacer le tableau
-        msg[i] = (char) 0;   
+      Receiver = 0;
     }
+    Serial.println(NbBit);
   }
-  if (NbBit==1){
+  if (NbBit==1 && Receiver == 1){
     if (msg[0]== 'C' && msg[1]== 'N'){                                             // si les deux premières lettres = C et N
       NbBit++;                                                                     // si oui, continuer
+      Receiver = 0;
     }
     else if (msg[0]== 'D' && msg[1]== 'C'){                                        // si les deux premières lettres = D et C               
       NbBit++;                                                                     // si oui, continuer
+      Receiver = 0;
     }
     else{                                                                          // si non, recommencer
       NbBit=0;
-      for (int i = 0; i = sizeof(msg); i++)                                        // effacer le tableau
-        msg[i] = (char) 0;
+      Receiver = 0;
     }
+    Serial.println(NbBit);
   }
-  if (NbBit==2){
+  if (NbBit==2 && Receiver == 1){
     if (msg[0]== 'C' && msg[1]== 'N' && msg[2]== 'T'){                             // si les trois premières lettres = C, N et T (Trame de contrôle)              
       NbBit++;                                                                     // si oui, continuer
+      Receiver = 0;
     }
     else if (msg[0]== 'C' && msg[1]== 'N' && msg[2]== 'F'){                        // si les trois premières lettres = C, N et F (Trame de configuration)                   
       NbBit++;                                                                     // si oui, continuer
+      Receiver = 0;
     }
     else if (msg[0]== 'D' && msg[1]== 'C' && msg[2]== 'X'){                        // si les trois premières lettres = D, C et X (Trame de demande de déconnexion)                              
       NbBit++;                                                                     // si oui, continuer
+      Receiver = 0;
     }
     else{                                                                          // si non, recommencer
       NbBit=0;
-      for (int i = 0; i = sizeof(msg); i++)                                        // effacer le tableau
-        msg[i] = (char) 0;
+      Receiver = 0;
+    }
+    Serial.println(NbBit);
+  }
+  if (NbBit==3 && Receiver == 1){                                                  // si la trame est envoyé par la manette qu'on a connectée
+    if (ManetteAdresse == msg[3]){  
+      NbBit++;                                                                     // si oui, continuer
+      Receiver = 0;
+      Serial.println(NbBit);
+    }
+    else{                                                                          // si non, recommencer
+      NbBit=0;
+      Receiver = 0;
     }
   }
-  if (NbBit==3 && ManetteAdresse == msg[3]){                                       // si la trame est envoyé par la manette qu'on a connectée
-    NbBit++;                                                                       // si oui, continuer
-  }
-  else{                                                                            // si non, recommencer
-    NbBit=0;
-    for (int i = 0; i = sizeof(msg); i++)                                          // effacer le tableau
-      msg[i] = (char) 0;
-  }
-  if (NbBit==4){                                                                   
+  if (NbBit==4 && Receiver == 1){                                                                   
     if (msg[0]== 'D' && msg[1]== 'C' && msg[2]== 'X'){                             // Pour la trame de demande de déconnexion
       if (msg[4] == '4'){                                                          // si le contrôle a reçu toute la trame
         Connexion = 0;                                                             // si oui, déconnecter
+        Serial.println("Connexion off");
       }
+    Receiver = 0;
     NbBit=0;                                                                       // recommencer peu importe si on a reçu toute la trame au complête
-    for (int i = 0; i = sizeof(msg); i++)                                          // effacer le tableau
-      msg[i] = (char) 0;
     }
     else{                                                                          // si la trame reçue n'est pas la trame de demande de déconnexion, continuer
       NbBit++;
-    } 
+      Receiver = 0;
+    }
   }
-  if (NbBit==5){
+  if (NbBit==5 && Receiver == 1){
     NbBit++;
+    Receiver = 0;
   }
-  if (NbBit==6){                                                                   
+  if (NbBit==6 && Receiver == 1){                                                                   
     if (msg[0]== 'C' && msg[1]== 'N' && msg[2]== 'T'){                             // Pour la trame de contrôle
       if (msg[6] == '6'){                                                          // si le contrôle a reçu toute la trame
         Vitesse = msg[4];                                                          // si oui, prendre en note la vitesse demandée
         ArretUrgence = msg[5];                                                     // prendre en note si l'arrêt d'urgence est demandé
+        Serial.println("Controle");
       }
+    Receiver = 0;
     NbBit = 0;                                                                     // recommencer peu importe si on a reçu toute la trame au complête
-    for (int i = 0; i = sizeof(msg); i++)                                          // effacer le tableau
-      msg[i] = (char) 0;  
     }
-    else{                                                                          // si la trame reçue n'est pas la trame de contrôle, continuer
+    else{                                                                          // si la trame reçue n'est pas la trame de demande de déconnexion, continuer
       NbBit++;
+      Receiver = 0;
     }
   }
-  if (NbBit==7){
-    NbBit++;
-  }
-  if (NbBit==8){                                                                   
-    if (msg[0]== 'C' && msg[1]== 'N' && msg[2]== 'F' && msg[8] == '8'){            // Pour la trame de configuration, si le contrôle a reçu toute la trame
+  if (NbBit==7 && Receiver == 1){                                                                   
+    if (msg[0]== 'C' && msg[1]== 'N' && msg[2]== 'F' && msg[7] == '7'){            // Pour la trame de configuration, si le contrôle a reçu toute la trame
       Acceleration = msg[4];                                                       // si oui, prendre en note l'accélération requise
-      Deceleration = msg[5];                                                       // prendre en note la décélération requise
-      LongueurCable[0] = msg[6];                                                   // prendre en note la longueur du câble utilisé
-      LongueurCable[1] = msg[7];      
+      LongueurCable[0] = msg[5];                                                   // prendre en note la longueur du câble utilisé
+      LongueurCable[1] = msg[6];      
+      Serial.println("Configuration");
     }  
-    else{
-      NbBit = 0;                                                                   // recommencer peu importe si on a reçu toute la trame au complête
-      for (int i = 0; i = sizeof(msg); i++)                                        // effacer le tableau
-        msg[i] = (char) 0;
-    }
+    NbBit = 0;                                                                   // recommencer peu importe si on a reçu toute la trame au complête
+    Receiver = 0;
   }
+  
 }
+delay(1000);
+}
+
+void loop(){
+  ReceiveUpdate();
 }
