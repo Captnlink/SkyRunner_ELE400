5868f02604032993b55f1c19e11a45842974ac16
Wsmon <wantel21@hotmail.com>
2016-04-03 14:09:41 -0400

    Update etat_led.h
    ajout de la struct  etat_led pour gérer le temps du dernier flash statut  des erreurs

--- Control/etat_led.h
+++ Control/etat_led.h
@@ -1,18 +1,18 @@
 /**
     Project : CableCam_Chariot\n
     @file Etat_Led.h
-    @brief Gèrent la LED d'information selon les erreurs et warnings du systeme
+    @brief GÃ¨rent la LED d'information selon les erreurs et warnings du systeme
     @author Captnlink
 	@author Wsimon
 	
     @version 1.0
     @date March 20, 2016
-	Afin de visualiser les problèmes pouvant survenir à l'intérieur du controlleur
-	et les états à problèmes, cette class permet donc de gérer la LED.
+	Afin de visualiser les problÃ¨mes pouvant survenir Ã  l'intÃ©rieur du controlleur
+	et les Ã©tats Ã  problÃ¨mes, cette class permet donc de gÃ©rer la LED.
 
 
-	Il reste à vérifier la struct controller pour pouvoir utiliser les divers variables
-	et à mieux définir les états pour vérifier le statut 
+	Il reste Ã  vÃ©rifier la struct controller pour pouvoir utiliser les divers variables
+	et Ã  mieux dÃ©finir les Ã©tats pour vÃ©rifier le statut 
 	
 	bool SetEndOfCourse(t_ledstate* ledstate);	 // Etat: Chariot atteint Fin de course  -> led clignote entre le vert et le jaune
 bool SetCantGoSetPoint(t_ledstate* ledstate);
@@ -24,16 +24,16 @@ bool SetCantGoSetPoint(t_ledstate* ledstate);
 
 /**
     @class EtatLed EtatLed.h
-    @brief gère l'état de la LED d'information
+    @brief gÃ¨re l'Ã©tat de la LED d'information
     @author Captnlink
     @version 1.1
     @date March 20, 2016
-    Permet de gérer les informations du controlleur qui peuvent être afficher par la LED.
+    Permet de gÃ©rer les informations du controlleur qui peuvent Ãªtre afficher par la LED.
 	
 */
 
 
-//Délai du cycle pour regarder l'état des led et aussi délai de  clignotement
+//DÃ©lai du cycle pour regarder l'Ã©tat des led et aussi dÃ©lai de  clignotement
 #define DELAY_ETAT_LED 250
 
 // Statut des led pour les switch case pour fonction etat-led
@@ -50,7 +50,7 @@ bool SetCantGoSetPoint(t_ledstate* ledstate);
 /*
 
 
-int etat_update(int previous_led_flash); // va retourner la dernieère fois que la led a flash
+int etat_update(int previous_led_flash); // va retourner la dernieÃ¨re fois que la led a flash
 
 
 
@@ -59,42 +59,45 @@ bool SetBattTooHot(t_ledstate* ledstate);	 // Etat batterie trop chaude -> led  
 bool SetObjectDetected(t_ledstate* ledstate); // Etat: objecte detecte par capteur -> led clignote entre le vert et le rouge
 bool SetEndOfCourse(t_ledstate* ledstate);	 // Etat: Chariot atteint Fin de course  -> led clignote entre le vert et le jaune
 bool SetCantGoSetPoint(t_ledstate* ledstate); // Etat: chariot ne peut aller au point voulu -> led  clignote en jaune
-bool SetEmergencyStop(t_ledstate* ledstate);  //Etat: arrêt d'urgence -> led reste en rouge 
-bool SetNoComms(t_ledstate* ledstate);		 // Etat: le module controle ne reçoit plus de donnée de la manette-> led reste en jaune
+bool SetEmergencyStop(t_ledstate* ledstate);  //Etat: arrÃªt d'urgence -> led reste en rouge 
+bool SetNoComms(t_ledstate* ledstate);		 // Etat: le module controle ne reÃ§oit plus de donnÃ©e de la manette-> led reste en jaune
 
 
 */
-
-
-
+typedef struct {
+	int m_etape_flash;
+	int m_etape_erreur;
+	int previous_led_flash;
+	bool statut_erreur; // false= tout va bien /true= il y a des problemes dÃ©tectes 
+}t_etat_led_control;
 
 
 		/*
 fn SetBattFaible
-Description:La fonction permet de vérifier si la batterie est faible ou non.
+Description:La fonction permet de vÃ©rifier si la batterie est faible ou non.
 
-Paramètre d'entrée:  tension lue de la batterie 
-Donnée retour de la fonction: statut de l'état batterie_faible (Vrai= la batterie est faible/Faux= la batterie n'est pas faible)
+ParamÃ¨tre d'entrÃ©e:  tension lue de la batterie 
+DonnÃ©e retour de la fonction: statut de l'Ã©tat batterie_faible (Vrai= la batterie est faible/Faux= la batterie n'est pas faible)
 */
 	bool SetBattFaible(double tension_batterie);
 
 
 		/*
 fn SetBattTooHot
-Description:La fonction permet de vérifier si la température de la batterie est trop chaude
+Description:La fonction permet de vÃ©rifier si la tempÃ©rature de la batterie est trop chaude
 
 
-Paramètre d'entrée: temperature lu de la battrie
-Donnée retour de la fonction: variable boolean (Vrai=batteriee est trop chaude/Faux= temperature de la batterie est ok)
+ParamÃ¨tre d'entrÃ©e: temperature lu de la battrie
+DonnÃ©e retour de la fonction: variable boolean (Vrai=batteriee est trop chaude/Faux= temperature de la batterie est ok)
 */
 	bool SetBattTooHot(double temp_batterie);
 
 		/*
 fn SetObjectDetected
-Description:La fonction permet de vérifier  si les capteurs ont  détecté
+Description:La fonction permet de vÃ©rifier  si les capteurs ont  dÃ©tectÃ©
 
-Paramètre d'entrée:  distance_objet_detecte     valeur 0 si rien n'a été detecté par les capteurs
-Donnée retour de la fonction: variable boolean (Vrai= un objet a été detecté/Faux= aucun n'obstacle a été detecté)
+ParamÃ¨tre d'entrÃ©e:  distance_objet_detecte     valeur 0 si rien n'a Ã©tÃ© detectÃ© par les capteurs
+DonnÃ©e retour de la fonction: variable boolean (Vrai= un objet a Ã©tÃ© detectÃ©/Faux= aucun n'obstacle a Ã©tÃ© detectÃ©)
 */
 	bool SetObjectDetected(double distance_objet_detecte);
 
@@ -102,43 +105,43 @@ Donnée retour de la fonction: variable boolean (Vrai= un objet a été detecté/Fau
 
 		/*
 fn SetEndOfCourse
-Description:La fonction permet de vérifier si le chariot a atteint la fin de course
+Description:La fonction permet de vÃ©rifier si le chariot a atteint la fin de course
 
-Paramètre d'entrée:  
-Donnée retour de la fonction: variable boolean (Vrai= le chariot a atteint la fin de course/Faux= le chariot n'a pas atteint la fin de course)
+ParamÃ¨tre d'entrÃ©e:  
+DonnÃ©e retour de la fonction: variable boolean (Vrai= le chariot a atteint la fin de course/Faux= le chariot n'a pas atteint la fin de course)
 */
 
-	//bool SetEndOfCourse();// juste clarifier avec quoi l'évaluer
+	//bool SetEndOfCourse();// juste clarifier avec quoi l'Ã©valuer
 
 
 
 	/*
 fn SetCantGoSetPoint
-Description:La fonction permet de vérifier si on  peut  atteindre la position désirée 
+Description:La fonction permet de vÃ©rifier si on  peut  atteindre la position dÃ©sirÃ©e 
 
-Paramètre d'entrée:  
-Donnée retour de la fonction: variable boolean (Vrai= le chariot ne peux atteindre le point voulu/Faux= le chariot peut atteint le point voulu)
+ParamÃ¨tre d'entrÃ©e:  
+DonnÃ©e retour de la fonction: variable boolean (Vrai= le chariot ne peux atteindre le point voulu/Faux= le chariot peut atteint le point voulu)
 */
-//	bool SetCantGoSetPoint();// juste clarifier avec quoi l'évaluer
+//	bool SetCantGoSetPoint();// juste clarifier avec quoi l'Ã©valuer
 
 
 
 		/*
 fn  SetEmergencyStop
-Description:La fonction permet de vérifier si l'arrêt d'urgence a été enclenché
+Description:La fonction permet de vÃ©rifier si l'arrÃªt d'urgence a Ã©tÃ© enclenchÃ©
 
-Paramètre d'entrée:  statut de l'arret d'urgence
-Donnée retour de la fonction: variable boolean (Vrai= l'arret d'urgence a été enclenché/Faux= l'arret d'urgence n'a pas été enclenché)
+ParamÃ¨tre d'entrÃ©e:  statut de l'arret d'urgence
+DonnÃ©e retour de la fonction: variable boolean (Vrai= l'arret d'urgence a Ã©tÃ© enclenchÃ©/Faux= l'arret d'urgence n'a pas Ã©tÃ© enclenchÃ©)
 */
-	bool SetEmergencyStop(int arrêt_urgence);
+	bool SetEmergencyStop(int arrÃªt_urgence);
 
 
 	/*
 fn  SetNoComms
 Description:La fonction permet de retourner le statut de la communication avec la manettre
 
-Paramètre d'entrée:  le statut de connexion avec la manette 
-Donnée retour de la fonction: variable boolean (Vrai= Aucune communication avec l'interface/Faux= La communication est présente )
+ParamÃ¨tre d'entrÃ©e:  le statut de connexion avec la manette 
+DonnÃ©e retour de la fonction: variable boolean (Vrai= Aucune communication avec l'interface/Faux= La communication est prÃ©sente )
 */
 	bool SetNoComms(int statut_connexion_manette);//
 	
@@ -148,8 +151,8 @@ Donnée retour de la fonction: variable boolean (Vrai= Aucune communication avec 
 fn  etat_led
 Description:La fonction permet d'allumer la led qu'on veut, soit la couleur rouge, vert, jaune ou aucune couleur
 
-Paramètre d'entrée:  les constantes de couleur
-Donnée retour de la fonction: Aucune
+ParamÃ¨tre d'entrÃ©e:  les constantes de couleur
+DonnÃ©e retour de la fonction: Aucune
 */
 	void etat_led(int color);
 
@@ -158,8 +161,8 @@ Donnée retour de la fonction: Aucune
 
 			/*
 fn  etat_update
-Description:La fonction permet de vérifier les différents états par la suite, allumer la led en conséquence
-Les états sont
+Description:La fonction permet de vÃ©rifier les diffÃ©rents Ã©tats par la suite, allumer la led en consÃ©quence
+Les Ã©tats sont
 
 Etat: tout va bien ->  led reste en vert 
  Etat: Batterie faible -> led  clignote en vert
@@ -167,16 +170,16 @@ Etat: tout va bien ->  led reste en vert
  Etat: objecte detecte par capteur -> led clignote entre le vert et le rouge
 Etat: Chariot atteint Fin de course  -> led clignote entre le vert et le jaune
  Etat: chariot ne peut aller au point voulu -> led  clignote en jaune
-Etat: arrêt d'urgence -> led reste en rouge 
-Etat: le module controle ne reçoit plus de donnée de la manette-> led reste en jaune
+Etat: arrÃªt d'urgence -> led reste en rouge 
+Etat: le module controle ne reÃ§oit plus de donnÃ©e de la manette-> led reste en jaune
 
-Paramètre d'entrée:  Données du controlleur permettant d'évaluer les états
-Donnée retour de la fonction: Aucune
+ParamÃ¨tre d'entrÃ©e:  DonnÃ©es du controlleur permettant d'Ã©valuer les Ã©tats
+DonnÃ©e retour de la fonction: Aucune
 */
 	
 	
 	
-	void etat_update(int previous_led_flash/*, données controlleur*/); // va retourner la dernieère fois que la led a flash
-
+	void etat_update(t_etat_led_control* etat_led_control/*, donnÃ©es controlleur*/); // va retourner la dernieÃ¨re fois que la led a flash
+	t_etat_led_control initialiser_etat_led();
 
 #endif
